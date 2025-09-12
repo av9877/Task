e3e18f29599e67c2e37c0f10fef39b150f4daf83
Mohit <nodejsmohit@jcsoftwaresolution.com>
2025-09-12 17:48:09 +0530

    changes

--- src/pages/taskList/html.jsx
+++ src/pages/taskList/html.jsx
@@ -1,18 +1,23 @@
 import Table from "../../components/table";
 
-const Html = ({
-  handleDelete,
-  setEditIndex,
-  setData,
-  savedData,
-  navigate,
-}) => {
+const Html = ({ handleSearch, search,handleDelete, setEditIndex, setData, savedData, navigate }) => {
   return (
     <>
       <section>
         <div className="container mx-auto px-5">
           <div className="flex items-center justify-between gap-3">
-            <h3 className="font-semibold text-2xl">Listing</h3>
+            <div className="flex items-center mb-4  gap-3">
+              <h3 className="font-semibold text-2xl">Listing</h3>
+              <input
+                id="firstName"
+                name="firstName"
+                type="text"
+                onChange={handleSearch}
+                value={search}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
+                placeholder="Search"
+              />
+            </div>
             <button
               onClick={() => navigate("/tasklist/add")}
               className="bg-gray-600 rounded-lg px-3 py-1 text-[#fff] cursor-pointer hover:opacity-90"
--- src/pages/taskList/index.jsx
+++ src/pages/taskList/index.jsx
@@ -4,10 +4,13 @@ import Html from "./html";
 
 const TaskList = () => {
   const [savedData, setSavedData] = useState([]);
+  const [fullData, setFullData] = useState([]);
+  const [search, setSearch] = useState("");
   const navigate = useNavigate();
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("list"));
     if (Array.isArray(storedData)) {
+      setFullData(storedData);
       setSavedData(storedData);
     } else {
       setSavedData([]);
@@ -19,12 +22,22 @@ const TaskList = () => {
     console.log(item, "delete");
     const updatedList = savedData.filter((_, i) => i !== item);
     setSavedData(updatedList);
-     localStorage.setItem("list", JSON.stringify(updatedList));
+    localStorage.setItem("list", JSON.stringify(updatedList));
   };
-
+  // search
+  const handleSearch = (e) => {
+    const value = e.target.value;
+    setSearch(value);
+    const filteredItem = fullData.filter((item) =>
+      item.firstName.toLowerCase().includes(value)
+    );
+    setSavedData(filteredItem);
+  };
   return (
     <>
       <Html
+        handleSearch={handleSearch}
+        search={search}
         handleDelete={handleDelete}
         savedData={savedData}
         navigate={navigate}
